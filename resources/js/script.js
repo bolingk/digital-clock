88dc60fa5f77b155bff8883a901b86d1e031b534
Kyle Boling <kmboling@gmail.com>
2019-02-28 15:22:22 -0500

    stop and zero functions added with onclick

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,18 +1,40 @@
-var clockString = setInterval(clockTick, 1000);
+var clockString = setInterval(clockTick, 1000),
+	today;
 
+function getToday() {
+	today = new Date();
+}
 
-function clockTick() {
-	var today = new Date(),
-		hour = today.getHours(),
+function displayTime() {
+	var hour = today.getHours(),
 		min = today.getMinutes(),
-		sec = today.getSeconds();
-		text = zeroPadding(hour) + ":" + zeroPadding(min) + ":" + zeroPadding(sec);
+		sec = today.getSeconds(),
+		text = intPadding(hour) + ":" + intPadding(min) + ":" + intPadding(sec);
 	document.getElementById('time-string').innerText = text;
 }
 
-function zeroPadding(n) {
+function clockTick() {
+	getToday();
+	displayTime();
+}
+
+function stopTick() {
+	clearInterval(clockString);
+}
+
+function stopWatchZero() {
+	clearInterval(clockString);
+	getToday();
+	today.setHours(0);
+	today.setMinutes(0);
+	today.setSeconds(0);
+	displayTime();
+}
+
+function intPadding(n) {
     if (n < 10) {
     	n = "0" + n
     }
     return n;
 }
+
